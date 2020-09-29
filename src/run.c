63f634edf3ffd9befa98bb3b38f2154f24e09076
kita0203 <conventionover@gmail.com>
2020-09-29 17:20:15 +0900

    9/29

--- src/run.c
+++ src/run.c
@@ -84,6 +84,59 @@ static void print_hex(uint8_t *bytes, size_t bytes_len) {
     printf("\n");
 }
 
+void cryptb (unsigned char *block, unsigned char *left, unsigned char *right, unsigned char *cf, unsigned char *new_left)
+{
+    int i, j;
+    for (i = 0; i < rounds; i++) {
+        memcpy(left, block, block_size / 2);
+        memcpy(right, block + block_size / 2, block_size / 2);
+        cf = f(left, block_size / 2);/*ここをargon2に変える*/
+        for (j = 0; j < block_size / 2; j++) new_left[j] = right[j] ^ cf[j];
+        memcpy(right, left, block_size / 2);
+        memcpy(left, new_left, block_size / 2);
+        memcpy(block, left, block_size / 2);
+        memcpy(block + block_size / 2, right, block_size / 2);
+        if (cf) free(cf);
+    }
+}
+void decrypt(unsigned char *block,  unsigned char *left, unsigned char *right, unsigned char *cf, unsigned char *new_left)
+{
+    int i, j;
+    for (i = rounds - 1; i >= 0; i--) {
+        memcpy(left, block, block_size / 2);
+        memcpy(right, block + block_size / 2, block_size / 2);
+        cf = f(right, block_size / 2);/*ここをargon2に変える*/
+        for (j = 0; j < block_size / 2; j++) new_left[j] = left[j] ^ cf[j];
+        memcpy(left, right, block_size / 2);
+        memcpy(right, new_left, block_size / 2);
+        memcpy(block, left, block_size / 2);
+        memcpy(block + block_size / 2, right, block_size / 2);
+        if (cf) free(cf);
+    }
+}
+
+
+
+unsigned char *f(unsigned char *block, int size)
+{
+    int i;
+    unsigned char *cip = calloc(size, sizeof(unsigned char));
+    for (i = 0; i < size; i++) {
+        cip[i] = block[i] ^ key[i];/*鍵とSBOXの部分がfeistelのラウンド関数部分→ここをargon2にかえる→ここの部分がargon2の入力部分（メッセージ）*/
+    }
+     sbox(cip, size);
+     return cip;
+}
+
+
+void free_arr(unsigned char **a)
+{
+    if (a && *a) {
+        free(*a);
+        *a = NULL;
+    }
+}
+
 /*
 Runs Argon2 with certain inputs and parameters, inputs not cleared. Prints the
 Base64-encoded hash string
@@ -193,6 +246,8 @@ int main(int argc, char *argv[]) {
     size_t pwdlen;
     char pwd[MAX_PASS_LEN], *salt;
     /* feistel構造を入れる（右と左に分ける） */
+
+
     if (argc <= 2) {
         if (argc  < 2) {
             printf("Too few arguments\n");
@@ -215,12 +270,10 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
     unsigned char *block = calloc(block_size, sizeof(unsigned char));
-    unsigned char *key = calloc(key_size, sizeof(unsigned char));
     unsigned char *left = calloc(block_size / 2, sizeof(unsigned char));
     unsigned char *right = calloc(block_size / 2, sizeof(unsigned char));
     unsigned char *cf = NULL;
     unsigned char *new_left = calloc(block_size / 2, sizeof(unsigned char));
-    unsigned char **keys;
     unsigned char *p = NULL;
     char *str;
     int i, rc = 0, wc = 0, last_rc = 0;
@@ -230,6 +283,57 @@ int main(int argc, char *argv[]) {
         printf("Bad file name\n");
         exit(1);
     }
+    char c[3], *eptr = NULL;
+    str = getstr();
+    
+    
+    memset(block, '\n', block_size);/*いらないかも*/
+    p = block;
+    while ((rc = read(fd1, p, block_size - rc)) > 0) {
+            p+=rc;
+            if (rc == block_size) {
+                if (mode == 0) {
+                    cryptb(block,  left, right, cf, new_left);
+                } else decrypt(block, left, right, cf, new_left);
+                p = block;
+                wc =0;
+                while ((wc = write(fd2, p, block_size - wc)) > 0) {
+                    p+=wc;
+                    if (wc == block_size) {
+                        p = block;
+                        rc = 0;
+                        memset(block, '\n', block_size);
+                        break;
+                    }
+                }
+            }
+            last_rc = rc; 
+    }
+    if (last_rc != 0) {
+        if (mode == 0) {
+            cryptb(block,  left, right, cf, new_left);
+        } else decrypt(block,  left, right, cf, new_left);
+        p = block;
+        wc = 0;
+        while ((wc = write(fd2, p, block_size - wc)) > 0) {   
+            p+=wc;
+            if (wc == block_size) break;
+        }
+    }
+    free_arr(&block);
+    free_arr(&left);
+    free_arr(&right);
+    free_arr(&cf);
+    free_arr(&new_left);
+    if (str) {
+        free(str);
+        str = NULL;
+    }
+    close(fd1);
+    close(fd2);
+    
+
+
     /* ここまでfeistelの文*/
     if (argc < 2) {
         usage(argv[0]);
@@ -380,7 +484,7 @@ int main(int argc, char *argv[]) {
     run(outlen, pwd, pwdlen, salt, t_cost, m_cost, lanes, threads, type,
        encoded_only, raw_only, version);
 
-     /* ここにfeistel構造入れる*/
+     
      
 
     return ARGON2_OK;
